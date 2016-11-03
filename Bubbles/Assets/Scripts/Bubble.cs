6250bc4577e26d260afaad9e76c1626a181fdfdf
Rob Hopps <rob-hopps@hotmail.co.uk>
2016-11-03 16:10:56 +0000

    BUBBLES
    except maybe asteroids

--- Bubbles/Assets/Scripts/Bubble.cs
+++ Bubbles/Assets/Scripts/Bubble.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bubble : MonoBehaviour {
+public class Bubble : MonoBehaviour
+{
+    public GameObject originObject;
+    [SerializeField]
+    Sprite m_sprite;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    bool delicateBubbles = false;
+
+    [SerializeField]
+    int lifeCounter = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // trigger animations of sprite
+
+        if (col.gameObject.tag == "Player" || delicateBubbles)
+        {
+            if (--lifeCounter <= 0)
+            {
+                BubbleManager.instance.RemoveBubble(this);
+            }
+        }
+    }
+
+
+
 }
--- Bubbles/Assets/Scripts/BubbleManager.cs
+++ Bubbles/Assets/Scripts/BubbleManager.cs
@@ -1,15 +1,77 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
-public class BubbleManager : MonoBehaviour {
+public class BubbleManager : MonoBehaviour
+{
+    private static BubbleManager _instance;
+    public static BubbleManager instance { get { return _instance; } }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField] GameObject bubblePrefab;
+    List<Bubble> bubbleList;
+
+    [SerializeField] int massSpawnNo = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        _instance = this;
+        bubbleList = new List<Bubble>();
+    }
+
+    public void InstantiateBubble(GameObject origin, Vector2 pos, Vector2 size, float magnitude, Vector2 direction)
+    {
+        GameObject bubbleOBJ = Instantiate(bubblePrefab, pos, Quaternion.identity) as GameObject;
+        bubbleOBJ.transform.localScale = size;
+
+
+        Bubble bubble = bubbleOBJ.GetComponent<Bubble>();
+        bubble.originObject = origin;
+
+        Rigidbody2D rb = bubbleOBJ.GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * magnitude, ForceMode2D.Impulse);
+
+        bubbleList.Add(bubble);
+
+    }
+
+    IEnumerator SpawnMass()
+    {
+        for(int i = 0; i < massSpawnNo; ++i)
+        {
+            float size = Random.Range(0.5f, 2);
+            InstantiateBubble(gameObject, new Vector2(Random.Range(-10, 10), Random.Range(-5, 5)), new Vector2(size, size), Random.Range(0.5f, 2f), new Vector2(Random.Range(-10, 10), Random.Range(-10, 10)));
+
+            yield return null;
+        }
+    }
+
+    public void RemoveBubble(Bubble bubble)
+    {
+        bubbleList.Remove(bubble);
+        Destroy(bubble.gameObject);
+    }
+
+    public void RemoveAllBubbles()
+    {
+        for(int i = 0; i < bubbleList.Count; ++i)
+        {
+            Destroy(bubbleList[i].gameObject);
+        }
+
+        bubbleList.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            InstantiateBubble(gameObject, Vector2.zero, Vector2.one, 5, Vector2.up);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(SpawnMass());
+        }
+    }
 }
