44f7c98307eda85ac24b79b251e97ecc74beedd6
Ed-McD <edard_666@hotmail.co.uk>
2016-11-03 17:33:21 +0000

    Movement started

---
+++ Bubbles/Assets/Attacher.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacher : MonoBehaviour {
+
+    [HideInInspector]
+    public PlayerMovement parentScript;
+    public bool attached { get; private set; }
+    public Vector2 direction = Vector3.up;
+    public float moveSpeed = 10;
+    public float maxDistance = 20;
+    private float distanceTravelled = 0;
+	
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Translate(direction*moveSpeed * Time.fixedDeltaTime);
+        distanceTravelled += direction.magnitude * moveSpeed * Time.fixedDeltaTime;
+        if (distanceTravelled >= maxDistance)
+            parentScript.DeleteAttacher();
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag != "Player")
+        {
+            attached = true;
+        }
+    }
+}
--- Bubbles/Assets/Scripts/PlayerMovement.cs
+++ Bubbles/Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class ControlState
+{
+    public bool FIRE = false;
+    public bool DETACH = false;   
+    public Vector2 MOVEMENT = Vector2.zero;
+    public Vector2 AIM = Vector2.up;
+    
+}
+
 public class PlayerMovement : MonoBehaviour {
 
+    [SerializeField]
+    public GameObject attacherPrefab;
+    private GameObject attacherInstance;
+    private Attacher attacherScript;
+    [SerializeField]
+    private Rigidbody2D selfRB;
+    [SerializeField]
+    private bool useDebugInput;
+    public float controlSpeed = 5;
+    
+    private ControlState currControls = new ControlState();
+    private ControlState prevControls = new ControlState();
+    public Vector2 aimingDirection { get; private set; }
+
+    public void SetControlState(ControlState _new)
+    {
+        prevControls = currControls;
+        currControls = _new;        
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void Reset() { selfRB.velocity = Vector3.zero; SetControlState(new ControlState()); }
+    public void Reset(Vector2 _spawnPos) { selfRB.velocity = Vector3.zero; SetControlState(new ControlState()); transform.position = _spawnPos; }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = useDebugInput ? Input.GetAxis("Horizontal") : currControls.MOVEMENT.x;
+        x *= controlSpeed * Time.deltaTime;
+        selfRB.AddForce(new Vector2(x, 0));
+        aimingDirection = (useDebugInput ? GetMouseAim() : currControls.AIM).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+
+    }
+    void Fire()
+    {
+        DeleteAttacher();
+        attacherInstance = (GameObject)Instantiate(attacherPrefab, transform.position, Quaternion.identity);
+        attacherScript = attacherInstance.GetComponent<Attacher>();
+        attacherScript.parentScript = this;
+        attacherScript.direction = aimingDirection;
+    }
+    public void DeleteAttacher()
+    {
+        if (attacherInstance != null)
+            Destroy(attacherInstance);
+        attacherInstance = null;
+        attacherScript = null;
+    }
+
+    Vector2 GetMouseAim()
+    {
+        Vector2 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return new Vector2(temp.x - transform.position.x, temp.y - transform.position.y);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, (new Vector3(transform.position.x + aimingDirection.x * 5, transform.position.y + aimingDirection.y * 5, 0)));
+    }
 }
